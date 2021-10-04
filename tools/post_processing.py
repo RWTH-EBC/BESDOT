461491fe990d948df32e040b087e04b98547aea9
yunqiucao <yunqiu.cao@eonerc.rwth-aachen.de>
2021-10-04 17:20:43 +0200

    Update post_processing.py

--- tools/post_processing.py
+++ tools/post_processing.py
@@ -115,28 +115,24 @@ def plot_step_profile(energy_type, demand, profile, time_step):
 
     if energy_type == 'heat':
         sink_tuple = heat_sink_tuple
-        df.plot(color=['#ffcccc'])
-        plt.legend(handles[::-1], labels[::-1], labels=['Heat'],
-                   loc='upper right')
+        ax.plot(time_steps, demand, color='#ffcccc', label='Heat')
     elif energy_type == 'elec':
         sink_tuple = elec_sink_tuple
-        df.plot(color=['#beffd3'])
-        plt.legend(handles[::-1], labels[::-1], labels=['Electricity'],
-                   loc='upper right')
+        ax.plot(time_steps, demand, color='#beffd3', label='Electricity')
     else:
         sink_tuple = None
 
-'''order_heat = 1.5
-    for device in df.columns:
-        if not str(device).endswith(sink_tuple):
+order_heat = 1.5
+    for device in profile:
+        if not device.endswith(sink_tuple):
          accumulate_series += profile[device]
          ax.step(time_steps, accumulate_series, where="post",
                    label=device, linewidth=2, zorder=order_heat)
          ax.fill_between(x_axis, accumulate_series,
                             step="post", zorder=order_heat)
          order_heat -= 0.1
-    for device in df.columns:
-        if str(device).endswith(sink_tuple):
+    for device in profile:
+        if device.endswith(sink_tuple):
             last_series_heat = copy.deepcopy(accumulate_series)
             accumulate_series -= profile[device]
             ax.step(time_steps, accumulate_series, where="post",
@@ -146,16 +142,18 @@ def plot_step_profile(energy_type, demand, profile, time_step):
                             hatch='///', alpha=0)
             order_heat -= 0.1
     ax.step(time_steps, demand, where="post", label='Bedarf', linestyle='--',
-            linewidth=2, zorder=1.5)'''
-    plt.ylabel('Leistung in kW')
-    plt.xlim(0, 23)
-    plt.ylim(0, None)
-    plt.xlabel('Stunde')
+            linewidth=2, zorder=1.5)
+            
+    ax.set_ylabel('Leistung in kW')
+    ax.set_xlim(0, 23)
+    ax.set_ylim(0, None)
+    ax.set_xlabel('Stunde')
 
-
-    plt.grid(axis="y", alpha=0.6)
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles[::-1], labels[::-1], loc='upper right')
+    ax.grid(axis="y", alpha=0.6)
     ax.set_axisbelow(True)
-    plt.suptitle(t='hourly profile', fontsize=18)
+    fig.suptitle(t='hourly profile', fontsize=18)
     plt.show()
 
     # plot_path = os.path.join(OUTPUTS_PATH, str(datetime.datetime.now(
