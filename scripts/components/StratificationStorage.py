129b88ce8ebc53cd01e04b3a745beac6191616dd
yunqiucao <yunqiu.cao@eonerc.rwth-aachen.de>
2021-10-20 17:09:38 +0200

    Update StratificationStorage.py

--- scripts/components/StratificationStorage.py
+++ scripts/components/StratificationStorage.py
@@ -83,11 +83,12 @@ class StratificationStorage(HotWaterStorage):
                         loss_var[len(model.time_step)])
         
         for t in range(len(model.time_step) - 1):
-            heat_water_percent[t + 2] = size * water_density * \
+            heat_water_percent[t + 2] = (size * water_density * \
                                         heat_water_percent[t + 1] - \
                                         mass_flow_var[t + 1] * t + \
                                         input_energy[t + 1] * t / \
-                                        water_heat_cap / unit_switch
+                                        water_heat_cap / unit_switch) / (size \
+                                        * water_density) 
             
         def _constraint_loss(self, model, loss_type='off'):
             """
