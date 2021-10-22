5a458655a3b1f94e7139cbd16f8a0b76740b0459
yunqiucao <yunqiu.cao@eonerc.rwth-aachen.de>
2021-10-22 10:54:03 +0200

    Update StratificationStorage.py

--- scripts/components/StratificationStorage.py
+++ scripts/components/StratificationStorage.py
@@ -71,85 +71,72 @@ class StratificationStorage(HotWaterStorage):
                            unit_switch + (return_temp_var[t + 2] -
                                           return_temp_var[t + 1]) *
                            water_density *
-                           size * (1 - heat_water_percent[t + 1]) * water_heat_cap /
+                           size * (1 - heat_water_percent[t + 1]) *
+                           water_heat_cap /
                            unit_switch ==
                            input_energy[t + 1] - output_energy[t + 1] -
                            loss_var[t + 1])
 
+        for t in range(len(model.time_step)):
+            model.cons.add(output_energy[t+1] ==
+                           (temp_var[t+1] - return_temp_var[t+1]) *
+                           mass_flow_var[t+1] * water_heat_cap / unit_switch)
+
         for t in range(len(model.time_step) - 1):
-            model.cons.add(heat_water_percent[t + 2] == (size * water_density
-                           * heat_water_percent[t + 1] - mass_flow_var[t + 1]
-                           * t + input_energy[t + 1] * t / (water_heat_cap * (
-                           temp_var[t + 1] - return_temp_var[t + 1]) /
-                           unit_switch) / (size * water_density)))
+            model.cons.add(heat_water_percent[t + 2] == (heat_water_percent[t +
+                           1] - (mass_flow_var[t + 1])/(size * water_density)
+                           + input_energy[t + 1] / (water_heat_cap * (
+                           temp_var[t + 1] - return_temp_var[t + 1]) * (size *
+                           water_density)) / unit_switch))
 
-        def _constraint_loss(self, model, loss_type='off'):
-            """
-            According to loss_type choose the wanted constraint about energy loss
-            of water tank.
-            'off': no energy loss occurs
-            """
-            loss_var = model.find_component('loss_' + self.name)
-            temp_var = model.find_component('temp_' + self.name)
-            return_temp_var = model.find_component('return_temp_' + self.name)
-
-            if loss_type == 'off':
-                for t in range(len(model.time_step)):
-                    model.cons.add(loss_var[t + 1] == 0)
-            else:
-                # FIXME: The energy loss equation shouldn't be like the following
-                #  format, which is only used for validation.
-                # FiXME (yni): mindesten should be loss determined by the device
-                #  size
-                for t in range(len(model.time_step)):
-                    model.cons.add(output_energy[t + 1] ==
-                                   (temp_var[t + 1] - return_temp_var[t + 1]) *
-                                   mass_flow_var[
-                                       t + 1] * water_heat_cap / unit_switch)
-
-    def _constraint_temp(self, model):
-        # Initial temperature for water in storage is define with a constant
-        # value, maybe later could change to others, like air temperature at
-        # time step 0.
-        temp_var = model.find_component('temp_' + self.name)
-        return_temp_var = model.find_component('return_temp_' + self.name)
+    def _constraint_loss(self, model, loss_type='off'):
+        """
+        According to loss_type choose the wanted constraint about energy loss
+        of water tank.
+        'off': no energy loss occurs
+        """
         loss_var = model.find_component('loss_' + self.name)
-        mass_flow_var = model.find_component('mass_flow_' + self.name)
+        temp_var = model.find_component('temp_' + self.name)
 
-        # tank的初始温度
-        model.cons.add(temp_var[1] == 60)
-        # tank的回水温度
-        model.cons.add(return_temp_var[1] == 20)
-        model.cons.add(mass_flow_var[1] == 0)
-        # 质量的初始流量
+        if loss_type == 'off':
+            for t in range(len(model.time_step)):
+                model.cons.add(loss_var[t + 1] == 0)
+        else:
+            # FIXME: The energy loss equation shouldn't be like the following
+            #  format, which is only used for validation.
+            # FiXME (yni): mindesten should be loss determined by the device
+            #  size
+            for t in range(len(model.time_step)):
+                model.cons.add(loss_var[t + 1] == 1.5 * ((temp_var[t + 1] -
+                                                          20) / 1000))
 
-        for t in model.time_step:
-            model.cons.add(temp_var[t] <= self.max_temp)
-            # model.cons.add(temp_var[t] >= self.min_temp)
-            model.cons.add(return_temp_var[t] <= self.max_temp)
-            # model.cons.add(return_temp_var[t] >= self.min_temp)
-            # 出水温度等于上层温度
+    def _constraint_temp(self, model, init_temp=60):
+        # Initial temperature for water in storage is define with a constant
+        # value.
+        temp_var = model.find_component('temp_' + self.name)
+        model.cons.add(temp_var[1] == init_temp)
 
-    def _constraint_return_temp(self, model):
+    def _constraint_return_temp(self, model, init_temp=20):
         # The first constraint for return temperature. Assuming a constant
         # temperature difference between flow temperature and return
         # temperature.
+        temp_var = model.find_component('temp_' + self.name)
+        return_temp_var = model.find_component('temp_' + self.name)
+        model.cons.add(return_temp_var[1] == init_temp)
         delta_temp = 20  # unit K
         min_delta_temp = 0
-        temp_var = model.find_component('temp_' + self.name)
-        # 回水温度出水温度温差小于最大温差
-        delta_temp = 20  # unit K
-        min_delta_temp = 0
-        temp_var = model.find_component('temp_' + self.name)
-        return_temp_var = model.find_component('return_temp_' + self.name)
-
         for t in model.time_step:
             model.cons.add(temp_var[t] - return_temp_var[t] <= delta_temp)
             model.cons.add(temp_var[t] - return_temp_var[t] >= min_delta_temp)
 
-    def _constraint_heat_water_percent(self, model):
+    def _constraint_mass_flow(self, model, init_massflow=0):
+        mass_flow_var = model.find_component('mass_flow_' + self.name)
+        model.cons.add(mass_flow_var[1] == init_massflow)
+
+    def _constraint_heat_water_percent(self, model, init_hwp=1.0):
         heat_water_percent = model.find_component('heat_water_percent_' +
                                                   self.name)
+        model.cons.add(heat_water_percent[1] == init_hwp)
         for t in range(len(model.time_step) - 1):
             model.cons.add(heat_water_percent[t + 1] >= 0)
             model.cons.add(heat_water_percent[t + 1] <= 1)
@@ -210,6 +197,8 @@ class StratificationStorage(HotWaterStorage):
         self._constraint_vdi2067(model)
         self._constraint_input_permit(model)
         self._constraint_heat_water_percent(model)
+        self._constraint_mass_flow(model)
+        self._constraint_loss(model, loss_type='on')
 
     def add_vars(self, model):
         super().add_vars(model)
@@ -229,7 +218,6 @@ class StratificationStorage(HotWaterStorage):
         heat_water_percent = pyo.Var(model.time_step, bounds=(0, None))
         model.add_component('heat_water_percent_' + self.name,
                             heat_water_percent)
-
        
 
 
