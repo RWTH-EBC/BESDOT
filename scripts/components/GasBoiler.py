5a392f932da61751e71deb2bc2b2b0c9ab0ab84b
Nie <yni@eonerc.rwth-aachen.de>
2021-11-03 09:12:43 +0100

    delete the maxpower constraint for boiler

--- scripts/components/GasBoiler.py
+++ scripts/components/GasBoiler.py
@@ -14,11 +14,11 @@ class GasBoiler(Component):
                          max_size=max_size,
                          current_size=current_size)
 
-    def _constraint_maxpower(self, model):
-        output_powers = model.find_component('output_' +
-                                             self.outputs[0] + '_' +
-                                             self.name)
-        size = model.find_component('size_' + self.name)
-
-        for t in model.time_step:
-            model.cons.add(output_powers[t] == size)
+    # def _constraint_maxpower(self, model):
+    #     output_powers = model.find_component('output_' +
+    #                                          self.outputs[0] + '_' +
+    #                                          self.name)
+    #     size = model.find_component('size_' + self.name)
+    #
+    #     for t in model.time_step:
+    #         model.cons.add(output_powers[t] == size)
