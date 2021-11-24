b505a736d8bb78f88bf6d97603a21865b8065027
Nie <yni@eonerc.rwth-aachen.de>
2021-11-24 17:03:15 +0100

    daily update

--- scripts/components/HeatExchangerTemp.py
+++ scripts/components/HeatExchangerTemp.py
@@ -31,7 +31,6 @@ class HeatExchangerTemp(Component):
         network, runs usually in counter flow.
         """
         water_heat_cap = 4.18 * 10 ** 3  # Unit J/kgK
-        water_density = 1000  # kg/m3
         unit_switch = 3600 * 1000  # J/kWh
 
         input_energy = model.find_component('input_' + self.inputs[0] +
@@ -47,14 +46,15 @@ class HeatExchangerTemp(Component):
         mass_flow_c = model.find_component('mass_flow_c_' + self.name)
         loss_var = model.find_component('loss_' + self.name)
 
-        for t in range(len(model.time_step)-1):
+        for t in range(len(model.time_step)):
             model.cons.add(input_energy[t+1] * (1 - loss_var[t+1]) ==
                            output_energy[t+1])
-
-        for t in range(len(model.time_step)):
-            model.cons.add(output_energy[t+1] ==
-                           (temp_var[t+1] - return_temp_var[t+1]) *
-                           mass_flow_var[t+1] * water_heat_cap / unit_switch)
+            model.cons.add(output_energy[t+1] == (temp_c[t+1] -
+                                                  return_temp_c[t+1]) *
+                           mass_flow_c[t+1] * water_heat_cap / unit_switch)
+            model.cons.add(input_energy[t+1] == (temp_h[t+1] -
+                                                 return_temp_h[t+1]) *
+                           mass_flow_h[t+1] * water_heat_cap / unit_switch)
 
     def _constraint_loss(self, model, loss_type='off'):
         """
@@ -89,14 +89,30 @@ class HeatExchangerTemp(Component):
                 model.cons.add(loss_var[t + 1] == 1.5 * ((temp_var[t + 1] -
                                                           20) / 1000))
 
-    def _constraint_heat_transfer(self, model):
+    def _constraint_delta_temp(self, model):
         """
-        The simulation of heat transfer according to heat exchanger working
-        principle for counter flow. Attention this is only possible with 4
-        temperature variables (primary inflow and return flow, secondary
-        inflow and return flow).
+        Calculation the heat flow with temperature difference between hot
+        medium and cool medium.
+        Q = k * A * delta_T
+        delta_T could be calculated with arithmetic mean value or logarithmic
+        mean value. Taking into account the complexity of model, the arithmetic
+        mean value is used. If necessary could change to logarithmic mean value.
         """
-        pass
+        temp_h = model.find_component('temp_h_' + self.name)
+        return_temp_h = model.find_component('return_temp_h_' + self.name)
+        temp_c = model.find_component('temp_c_' + self.name)
+        return_temp_c = model.find_component('return_temp_c_' + self.name)
+        delta_temp = model.find_component('delta_temp_' + self.name)
+
+        input_energy = model.find_component('input_' + self.inputs[0] +
+                                            '_' + self.name)
+
+        for t in range(len(model.time_step)):
+            model.cons.add(delta_temp == (temp_h + return_temp_h - temp_c -
+                                          return_temp_c) / 2)
+            model.cons.add(input_energy[t+1] == (temp_h[t + 1] -
+                                                   return_temp_h[t + 1]) *
+                           mass_flow_h[t + 1] * water_heat_cap / unit_switch)
 
     def add_vars(self, model):
         super().add_vars(model)
@@ -124,6 +140,10 @@ class HeatExchangerTemp(Component):
 
         mass_flow_c = pyo.Var(model.time_step, bounds=(0, None))
         model.add_component('mass_flow_c_' + self.name, mass_flow_c)
+
+        # Temperature difference value
+        delta_temp = pyo.Var(model.time_step, bounds=(0, None))
+        model.add_component('delta_temp_' + self.name, delta_temp)
 
         # The energy loss is considered, because of the heat transfer to the
         # environment.
