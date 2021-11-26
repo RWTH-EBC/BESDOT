241881b7a1594e04d3d00ec105dfef92ba17bd92
Yunqiu <yunqiu.cao@rwth-aachen.de>
2021-11-26 16:02:04 +0100

    add tools calc_exhaust_gas_loss

--- tools/calc_exhaust_gas_loss.py
+++ tools/calc_exhaust_gas_loss.py
@@ -10,33 +10,34 @@ path = os.path.join(base_path, "data", "component_database",
 
 def calc_exhaust_gas_loss(path, file_name):
     data = pd.read_excel(path, sheet_name='INPUT')
-    df = pd.DataFrame(columns=['Abgastemp', 'Luftemp', 'A1', 'B', 'C', 'H',
-                               'O', 'N', 'Luftzahl', 'v0', 'vCO2', 'vAbgas',
-                               'vCO2_pre', 'Abgasverlust'], index=[])
-    abgas_luft_1 = 0
+    df = pd.DataFrame(columns=['exhaustgastemp', 'airtemp', 'A1', 'B', 'C', 'H',
+                               'O', 'N', 'airnumber', 'v0', 'vCO2',
+                               'vexhaustgas',
+                               'vCO2_pre', 'exhaustgasloss'], index=[])
+    airnumber_1 = 0
     for i in range(len(data)):
         a = data.iloc[i]
         v0 = (1/0.21)*(1.866*a['C']+5.56*a['H']-0.7*a['O'])
         a['v0'] = v0
         vCO2 = a['C'] * 1.866
         a['vCO2'] = vCO2
-        if a['Luftzahl'] == 1:
-            abgas_luft_1 = vCO2+(0.79*v0+0.8*a['N'])+(11.1*a['H']+0.016*v0)
-            vAbgas = vCO2+(0.79*v0+0.8*a['N'])+(11.1*a['H']+0.016*v0)
-        if a['Luftzahl'] != 1:
-            vAbgas = abgas_luft_1 + (a['Luftzahl']-1) * a['v0'] + 0.016 * \
-                     (a['Luftzahl']-1) * a['v0']
-        a['vAbgas'] = vAbgas
-        vCO2_pre = vCO2/vAbgas
+        if a['airnumber'] == 1:
+            airnumber_1 = vCO2+(0.79*v0+0.8*a['N'])+(11.1*a['H']+0.016*v0)
+            vexhaustgas = vCO2+(0.79*v0+0.8*a['N'])+(11.1*a['H']+0.016*v0)
+        if a['airnumber'] != 1:
+            vexhaustgas = airnumber_1 + (a['airnumber']-1) * a['v0'] + 0.016 * \
+                     (a['airnumber']-1) * a['v0']
+        a['vexhaustgas'] = vexhaustgas
+        vCO2_pre = vCO2/vexhaustgas
         a['vCO2_pre'] = vCO2_pre
-        Abgasverlust = (a['Abgastemp']-a['Luftemp'])*(a['A1']/(vCO2_pre*100) +
-                                                      a['B'])
-        a['Abgasverlust'] = Abgasverlust
+        exhaustgasloss = (a['exhaustgastemp']-a['airtemp'])*(a['A1']/(
+                vCO2_pre*100) + a['B'])
+        a['exhaustgasloss'] = exhaustgasloss
         size = df.index.size
-        df.loc[size] = [a['Abgastemp'], a['Luftemp'], a['A1'], a['B'], a['C'],
-                        a['H'], a['O'], a['N'], a['Luftzahl'], a['v0'],
-                        a['vCO2'], a['vAbgas'], a['vCO2_pre'],
-                        a['Abgasverlust']]
+        df.loc[size] = [a['exhaustgastemp'], a['airtemp'], a['A1'], a['B'],
+                        a['C'], a['H'], a['O'], a['N'], a['airnumber'], a['v0'],
+                        a['vCO2'], a['vexhaustgas'], a['vCO2_pre'],
+                        a['exhaustgasloss']]
     df.to_excel(file_name, sheet_name='OUTPUT')
 
 
