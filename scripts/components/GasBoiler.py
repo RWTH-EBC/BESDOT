9ede0b491f87b066bd8500568e7ab3519bf65639
Nie <yni@eonerc.rwth-aachen.de>
2021-12-06 12:32:55 +0100

    fix tiny bug in GasBoiler

--- scripts/components/GasBoiler.py
+++ scripts/components/GasBoiler.py
@@ -1,9 +1,10 @@
+import os
 import warnings
-import pyomo.environ as pyo
+import pandas as pd
 from scripts.Component import Component
-import warnings
 
 base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 class GasBoiler(Component):
     def __init__(self, comp_name, comp_type="GasBoiler", comp_model=None,
@@ -32,6 +33,3 @@ class GasBoiler(Component):
         else:
             warnings.warn("In the model database for " + self.component_type +
                           " lack of column for exhaust gas loss.")
-
-
-
