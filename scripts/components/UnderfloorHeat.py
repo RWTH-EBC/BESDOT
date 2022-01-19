e8c7c4d46b64d1c4cf46ef902d7de07b5eb82240
[yunqiu] <[yunqiu.cao@eonerc.rwth-aachen.de]>
2022-01-19 14:50:00 +0100

    topology of underfloorheat

--- scripts/components/UnderfloorHeat.py
+++ scripts/components/UnderfloorHeat.py
@@ -1,18 +1,51 @@
 from scripts.Component import Component
+import warnings
+import pyomo.environ as pyo
+from scripts.FluidComponent import FluidComponent
 
 
-class UnderfloorHeat(Component):
-    """
-    pass
-    """
+class UnderfloorHeat(FluidComponent):
     def __init__(self, comp_name, comp_type="UnderfloorHeat", comp_model=None,
                  min_size=0, max_size=1000, current_size=0):
-        self.inputs = ['heat']
-        self.outputs = ['heat']  # todo: same as Radiator
-
         super().__init__(comp_name=comp_name,
                          comp_type=comp_type,
                          comp_model=comp_model,
                          min_size=min_size,
                          max_size=max_size,
                          current_size=current_size)
+
+    def _constraint_conver(self, model):
+        pass
+
+    def _constraint_temp(self, model, init_temp=30):
+        # Initial temperature for water in storage is define with a constant
+        # value.
+        temp_var = model.find_component('temp_' + self.name)
+        model.cons.add(temp_var[1] == init_temp)
+
+        for heat_input in self.heat_flows_in:
+            t_out = model.find_component(heat_input[1] + '_' + heat_input[0] +
+                                         '_' + 'temp')
+            for t in range(len(model.time_step)):
+                model.cons.add(temp_var[t + 1] == t_out[t + 1])
+
+        for heat_output in self.heat_flows_out:
+            t_out = model.find_component(heat_output[0] + '_' + heat_output[1] +
+                                         '_' + 'temp')
+            for t in range(len(model.time_step)):
+                model.cons.add(temp_var[t + 1] == t_out[t + 1])
+
+    def add_cons(self, model):
+        self._constraint_conver(model)
+        self._constraint_temp(model)
+        self._constraint_mass_flow(model)
+        self._constraint_heat_inputs(model)
+        self._constraint_heat_outputs(model)
+        self._constraint_vdi2067(model)
+
+    def add_vars(self, model):
+        super().add_vars(model)
+
+        temp = pyo.Var(model.time_step, bounds=(0, None))
+        model.add_component('temp_' + self.name, temp)
+
