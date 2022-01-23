98dcf4afcbdbbf772d07b0e4d0a0239ea85b74e5
[yunqiu] <[yunqiu.cao@eonerc.rwth-aachen.de]>
2022-01-23 21:27:34 +0100

    problem of Nonlinear

--- scripts/components/UnderfloorHeat.py
+++ scripts/components/UnderfloorHeat.py
@@ -54,7 +54,7 @@ class UnderfloorHeat(HeatExchangerFluid):
         self._constraint_mass_flow(model)
         self._constraint_heat_inputs(model)
         self._constraint_heat_outputs(model)
-        self._constraint_floor_temp(model)
+       # self._constraint_floor_temp(model)
         self._constraint_vdi2067(model)
 
     def add_vars(self, model):
