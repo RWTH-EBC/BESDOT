a83380516099710958be843159fde59df107860d
qiminli <qiminemail@gmail.com>
2022-03-03 22:44:03 +0100

    status_CHPFluidSmall

--- scripts/components/CHPFluidSmall.py
+++ scripts/components/CHPFluidSmall.py
@@ -1,13 +1,14 @@
 import warnings
 import pyomo.environ as pyo
 from scripts.FluidComponent import FluidComponent
+from scripts.components.CHP import CHP
 
-class CHPFluidSmall(FluidComponent):
+class CHPFluidSmall(CHP, FluidComponent):
 
     def __init__(self, comp_name, comp_type="CHPFluidSmall", comp_model=None,
                  min_size=0, max_size=50, current_size=0):
-        self.inputs = ['gas']
-        self.outputs = ['heat', 'elec']
+        #self.inputs = ['gas']
+        #self.outputs = ['heat', 'elec']
         super().__init__(comp_name=comp_name,
                          comp_type=comp_type,
                          comp_model=comp_model,
@@ -15,25 +16,25 @@ class CHPFluidSmall(FluidComponent):
                          max_size=max_size,
                          current_size=current_size)
 
+    # Pel = elektrische Nennleistung = comp_size
+    # Qth = thermische Nennleistung
     def _constraint_Pel(self, model):
         Pel = model.find_component('size_' + self.name)
         Qth = model.find_component('therm_size_' + self.name)
-        for t in model.time_step:
-            model.cons.add(Qth[t] == 2.1178 * Pel[t] + 2.5991)
+        model.cons.add(Qth == 2.1178 * Pel + 2.5991)
 
     def _constraint_therm_eff(self, model):
         Qth = model.find_component('therm_size_' + self.name)
         outlet_temp = model.find_component('outlet_temp_' + self.name)
         therm_eff = model.find_component('therm_eff_' + self.name)
         for t in model.time_step:
-            model.cons.add(therm_eff[t] == 0.705 - 0.0008 * (Qth[t] - 44) -
+            model.cons.add(therm_eff[t] == 0.705 - 0.0008 * (Qth - 44) -
                            0.006 * (outlet_temp[t] - 30))
 
     def _constraint_elec_eff(self, model):
         Pel = model.find_component('size_' + self.name)
         elec_eff = model.find_component('elec_eff_' + self.name)
-        for t in model.time_step:
-            model.cons.add(elec_eff[t] == (0.1016 * Pel[t] + 29.609) / 100)
+        model.cons.add(elec_eff == (0.1016 * Pel + 29.609) / 100)
 
     def _constraint_temp(self, model):
         outlet_temp = model.find_component('outlet_temp_' + self.name)
@@ -48,6 +49,8 @@ class CHPFluidSmall(FluidComponent):
             model.cons.add(inlet_temp[t] == t_in[t])
 
     def _constraint_conver(self, model):
+        Pel = model.find_component('size_' + self.name)
+        Qth = model.find_component('therm_size_' + self.name)
         therm_eff = model.find_component('therm_eff_' + self.name)
         elec_eff = model.find_component('elec_eff_' + self.name)
         input_energy = model.find_component('input_' + self.inputs[0] +
@@ -56,10 +59,13 @@ class CHPFluidSmall(FluidComponent):
             'output_' + self.outputs[0] + '_' + self.name)
         output_elec = model.find_component(
             'output_' + self.outputs[1] + '_' + self.name)
+        status = model.find_component('status_' + self.name)
 
         for t in model.time_step:
             model.cons.add(input_energy[t] * therm_eff[t] == output_heat[t])
-            model.cons.add(input_energy[t] * elec_eff[t] == output_elec[t])
+            model.cons.add(input_energy[t] * elec_eff == output_elec[t])
+            model.cons.add(Qth * status[t] == output_heat[t])
+            model.cons.add(Pel * status[t] == output_elec[t])
 
     def add_cons(self, model):
         self._constraint_Pel(model)
@@ -73,20 +79,20 @@ class CHPFluidSmall(FluidComponent):
     def add_vars(self, model):
         super().add_vars(model)
 
-        Pel = pyo.Var(model.time_step, bounds=(0, 50))
-        model.find_component('size_' + self.name, Pel)
-
-        Qth = pyo.Var(model.time_step, bounds=(0, None))
-        model.find_component('therm_size_' + self.name, Qth)
+        Qth = pyo.Var(bounds=(0, None))
+        model.add_component('therm_size_' + self.name, Qth)
 
         therm_eff = pyo.Var(model.time_step, bounds=(0, 1))
-        model.find_component('therm_eff_' + self.name, therm_eff)
+        model.add_component('therm_eff_' + self.name, therm_eff)
 
-        elec_eff = pyo.Var(model.time_step, bounds=(0, 1))
-        model.find_component('elec_eff_' + self.name, elec_eff)
+        elec_eff = pyo.Var(bounds=(0, 1))
+        model.add_component('elec_eff_' + self.name, elec_eff)
 
         outlet_temp = pyo.Var(model.time_step, bounds=(0, None))
         model.add_component('outlet_temp_' + self.name, outlet_temp)
 
         inlet_temp = pyo.Var(model.time_step, bounds=(0, None))
         model.add_component('inlet_temp_' + self.name, inlet_temp)
+
+        status = pyo.Var(model.time_step, domain=pyo.Binary)
+        model.add_component('status_' + self.name, status)
