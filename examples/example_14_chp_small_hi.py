632b872c160dcee4c721d3bde828993d9d7372c4
qiminli <qiminemail@gmail.com>
2022-03-06 13:58:45 +0100

    Ohne Brennwertnutzung

---
+++ examples/example_14_chp_small_hi.py
@@ -0,0 +1,54 @@
+import os
+from scripts.Project import Project
+from scripts.Environment import Environment
+from scripts.Building import Building
+import tools.post_processing as post_pro
+
+base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+################################################################################
+#                           Generate python objects
+################################################################################
+
+# Generate a project object at first.
+project = Project(name='project_14', typ='building')
+
+
+# Generate the environment object
+env_14 = Environment(time_step=3)
+project.add_environment(env_14)
+
+# If the objective of the project is the optimization for building, a building
+# should be added to the project.
+bld_14 = Building(name='bld_14', area=200)
+
+# Add the energy demand profiles to the building object
+# Attention! generate thermal with profile whole year temperature profile
+# bld_2.add_thermal_profile('heat', env_2.temp_profile_original, env_2)
+
+bld_14.demand_profile['heat_demand'] = [1, 0, 1]
+bld_14.demand_profile["elec_demand"] = [1, 0, 1]
+
+# Pre define the building energy system with the topology for different
+# components and add components to the building.
+# todo: 1 hinter 'chp_fluid_small' kann man löschen oder hinzufügen.
+# (keine Auslauftemperaturanforderung)
+topo_file = os.path.join(base_path, 'data', 'topology',
+                         'chp_fluid_small_hi.csv')
+bld_14.add_topology(topo_file)
+bld_14.add_components(project.environment)
+project.add_building(bld_14)
+
+################################################################################
+#                        Build pyomo model and run optimization
+################################################################################
+project.build_model(obj_typ='operation_cost')
+project.run_optimization('gurobi', save_lp=True, save_result=True)
+
+################################################################################
+#                                  Post-processing
+################################################################################
+
+result_output_path = os.path.join(base_path, 'data', 'opt_output',
+                                  project.name + '_result.csv')
+#post_pro.plot_all(result_output_path, time_interval=[0, env_14.time_step])
