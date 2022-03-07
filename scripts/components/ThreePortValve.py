e63f6ca07fc9da58e8c8527ce19c4e97a179351e
Nie <yni@eonerc.rwth-aachen.de>
2022-03-07 18:00:54 +0100

    fix small bug with constraint of temperature

--- scripts/components/ThreePortValve.py
+++ scripts/components/ThreePortValve.py
@@ -11,20 +11,26 @@ class ThreePortValve(HeatExchangerFluid):
 
     def _constraint_mix(self, model):
         """After flow mixing in three port valve, flow in output side should be
-         larger than the flow in input side"""
+         larger than the flow in input side. On the other hand, the temperature
+         of fluid before and after division remain unchanged."""
         heat_input = self.heat_flows_in[0]
         mass_flow_in = model.find_component(heat_input[0] + '_' +
                                             heat_input[1] + '_mass')
         heat_output = self.heat_flows_out[0]
         mass_flow_out = model.find_component(heat_output[1] + '_' +
                                              heat_output[0] + '_mass')
+        # Temperature of fluid before and after division
+        temp_flow_in = model.find_component(heat_input[1] + '_' +
+                                            heat_input[0] + '_temp')
+        temp_flow_out = model.find_component(heat_output[0] + '_' +
+                                             heat_output[1] + '_temp')
 
         for t in model.time_step:
             model.cons.add(mass_flow_in[t] <= mass_flow_out[t])
+            model.cons.add(temp_flow_in[t] == temp_flow_out[t])
 
     def add_cons(self, model):
         self._constraint_heat_inputs(model)
         self._constraint_heat_outputs(model)
         self._constraint_conver(model)
-        self._constraint_unidirect(model)
         self._constraint_mix(model)
