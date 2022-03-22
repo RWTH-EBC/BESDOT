71be24bc328ffaf5643b2548f22181d058af8f4c
Nie <yni@eonerc.rwth-aachen.de>
2022-03-22 16:12:40 +0100

    change loss coefficient to 0.6

--- scripts/components/HomoStorage.py
+++ scripts/components/HomoStorage.py
@@ -85,7 +85,7 @@ class HomoStorage(FluidComponent, HotWaterStorage):
             #  but the hard coding values are not be validated. It should be
             #  got from a plausible resource
             for t in range(len(model.time_step)):
-                model.cons.add(loss_var[t + 1] == 1.5 * ((temp_var[t + 1] -
+                model.cons.add(loss_var[t + 1] == 0.6 * ((temp_var[t + 1] -
                                                           20) / 1000) * size)
 
     def _constraint_temp(self, model, init_temp=58):
