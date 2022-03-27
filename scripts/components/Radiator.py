94a0275ce18248731b833a9cbefcb34223663daf
[yunqiu] <[yunqiu.cao@eonerc.rwth-aachen.de]>
2022-03-27 13:21:24 +0200

    add topology final.csv

--- scripts/components/Radiator.py
+++ scripts/components/Radiator.py
@@ -11,7 +11,7 @@ unit_switch = 3600 * 1000  # J/kWh
 
 class Radiator(HeatExchangerFluid, FluidComponent):
     def __init__(self, comp_name, comp_type="Radiator", comp_model=None,
-                 min_size=0, max_size=1000, current_size=0, over_temp_n=49.8):
+                 min_size=0, max_size=1000, current_size=0):
         super().__init__(comp_name=comp_name,
                          comp_type=comp_type,
                          comp_model=comp_model,
@@ -43,8 +43,8 @@ class Radiator(HeatExchangerFluid, FluidComponent):
 
     def _constraint_temp(self, model, init_temp=45):
         temp_var = model.find_component('temp_' + self.name)
-        #for t in model.time_step:
-        #    model.cons.add(temp_var[t] == init_temp)
+        for t in model.time_step:
+            model.cons.add(temp_var[t] == init_temp)
         for heat_input in self.heat_flows_in:
             t_out = model.find_component(heat_input[0] + '_' + heat_input[1] +
                                          '_' + 'temp')
@@ -72,7 +72,7 @@ class Radiator(HeatExchangerFluid, FluidComponent):
         self._constraint_conver(model)
         self._constraint_temp(model)
         self._constraint_return_temp(model)
-        self._constraint_mass_flow(model)
+        #self._constraint_mass_flow(model)
         self._constraint_heat_inputs(model)
         self._constraint_vdi2067(model)
 
