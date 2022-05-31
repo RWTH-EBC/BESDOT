27d0732e798f11cd7fc0563baa8af099a1727feb
Xin4Yuji <xin.zhou@eonerc.rwth-aachen.de>
2022-05-31 16:45:36 +0200

    Dev cop xzh

--- examples/example_8_airheatpump_vd.py
+++ examples/example_8_airheatpump_vd.py
@@ -15,29 +15,29 @@ base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ################################################################################
 
 # Generate a project object at first.
-project = Project(name='project_2', typ='building')
+project = Project(name='project_8_2', typ='building')
 
 # Generate the environment object
-env_2 = Environment(time_step=3)
-project.add_environment(env_2)
+env_8 = Environment(time_step=500)
+project.add_environment(env_8)
 
 # If the objective of the project is the optimization for building, a building
 # should be added to the project.
-bld_2 = Building(name='bld_2', area=200)
+bld_8 = Building(name='bld_8', area=200)
 
 # Add the energy demand profiles to the building object
 # Attention! generate thermal with profile whole year temperature profile
 # bld_2.add_thermal_profile('heat', env_2.temp_profile_original, env_2)
 
-bld_2.demand_profile['heat_demand'] = [5, 3, 4]
-
+#bld_8.demand_profile['heat_demand'] = [1, 0, 5, 2] * 50
+bld_8.add_thermal_profile('heat', env_8.temp_profile_original, env_8)
 # Pre define the building energy system with the topology for different
 # components and add components to the building.
 topo_file = os.path.join(base_path, 'data', 'topology',
-                         'homostorage_sim.csv')
-bld_2.add_topology(topo_file)
-bld_2.add_components(project.environment)
-project.add_building(bld_2)
+                         'airheatpump_tp.csv')
+bld_8.add_topology(topo_file)
+bld_8.add_components(project.environment)
+project.add_building(bld_8)
 
 ################################################################################
 #                        Build pyomo model and run optimization
@@ -46,15 +46,11 @@ project.build_model(obj_typ='annual_cost')
 project.run_optimization('gurobi', save_lp=True, save_result=True)
 
 ################################################################################
-#                                  Post-processing
+result_output_path = os.path.join(base_path, 'data', 'opt_output',
+                                  project.name + '_result.csv')
+post_pro.plot_all(result_output_path, time_interval=[0, env_8.time_step])
 ################################################################################
 
 result_output_path = os.path.join(base_path, 'data', 'opt_output',
                                   project.name + '_result.csv')
-post_pro.plot_all(result_output_path, time_interval=[0, env_2.time_step])
-#post_pro.plot_double(result_output_path, "boi", "water_tes", 365, "gas",
-#                      "heat")
-#post_pro.plot_double(result_output_path, "conden_boi", "water_tes", 365, "gas",
-#                     "heat")
-#post_pro.plot_double(result_output_path, "water_tes", "therm_cns", 365, "heat",
-#                     "heat")
+#post_pro.plot_all(result_output_path, time_interval=[0, env_2.time_step])
