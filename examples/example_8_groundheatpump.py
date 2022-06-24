9cbec7ae841e1e3e665df2c096295d1c42d6409d
Xin <xin.zhou@eonerc.rwth-aachen.de>
2022-06-24 12:12:35 +0200

    cluster

--- examples/example_8_groundheatpump.py
+++ examples/example_8_groundheatpump.py
@@ -29,7 +29,7 @@ bld_8 = Building(name='bld_8', area=200, bld_typ='Wohngebäude')
 # bld_2.add_thermal_profile('heat', env_2.temp_profile_original, env_2)
 
 #bld_8.demand_profile['heat_demand'] = [1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0]
-bld_8.add_thermal_profile('heat', env_8.temp_profile, env_8)
+bld_8.add_thermal_profile('heat', env_8)
 # Pre define the building energy system with the topology for different
 # components and add components to the building.
 topo_file = os.path.join(base_path, 'data', 'topology',
